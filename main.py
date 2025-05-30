8456aefe994d0c3647614e8790763e516e2df2a8
Tomek <tomek12912@gmail.com>
2025-05-30 22:03:00 +0200

    Basic game with walls added

--- main.py
+++ main.py
@@ -11,8 +11,176 @@ class Actions(Enum):
     DOWN = 3
 
 class GridEnv(gym.Env):
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
     def __init__(self, render_mode=None, size=5):
         self.window_size = 512
         self.size = size
+        self.observation_space = spaces.Dict(
+            {
+                "agent" : spaces.Box(0, size-1, shape=(2,), dtype=int),
+                "target": spaces.Box(0, size - 1, shape=(2,), dtype=int),
+            }
+        )
+        self._agent_location = np.array([-1, -1], dtype=int)
+        self._target_location = np.array([-1, -1], dtype=int)
+        self.action_space = spaces.Discrete(4)
+        self._action_to_direction = {
+            Actions.RIGHT.value: np.array([1, 0]),
+            Actions.UP.value: np.array([0, 1]),
+            Actions.LEFT.value: np.array([-1, 0]),
+            Actions.DOWN.value: np.array([0, -1]),
+        }
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+        self.window = None
+        self.clock = None
+
+        self.walls = []
+
+    def _get_obs(self):
+        return {"agent": self._agent_location, "target": self._target_location}
+
+    def _get_info(self):
+        return {
+            "distance": np.linalg.norm(
+                self._agent_location - self._target_location, ord=1
+            )
+        }
+
+    def reset(self, seed=None, options=None):
+        super().reset(seed=seed)
+
+        self._agent_location = self.np_random.integers(0, self.size, size=2, dtype=int)
+        self._target_location = self._agent_location
+        while np.array_equal(self._target_location, self._agent_location):
+            self._target_location = self.np_random.integers(
+                0, self.size, size=2, dtype=int
+            )
+        while(len(self.walls) < 5):
+            wall_pos = tuple(self.np_random.integers(0, self.size, size=2, dtype=int))
+            if wall_pos != tuple(self._agent_location) and wall_pos != tuple(self._target_location) \
+                    and wall_pos not in self.walls:
+                self.walls.append(wall_pos)
+
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, info
+
+    def step(self, action):
+        direction = self._action_to_direction[action]
+        possible_move = np.clip(
+            self._agent_location + direction, 0, self.size - 1
+        )
+        if tuple(possible_move) not in self.walls:
+            self._agent_location = possible_move
+
+        terminated = np.array_equal(self._agent_location, self._target_location)
+        reward = 1 if terminated else -0.1
+        observation = self._get_obs()
+        info = self._get_info()
+
+        if self.render_mode == "human":
+            self._render_frame()
+
+        return observation, reward, terminated, False, info
+
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode(
+                (self.window_size, self.window_size)
+            )
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        pix_square_size = (
+                self.window_size / self.size
+        )  # The size of a single grid square in pixels
+
+        # First we draw the target
+        pygame.draw.rect(
+            canvas,
+            (255, 0, 0),
+            pygame.Rect(
+                pix_square_size * self._target_location,
+                (pix_square_size, pix_square_size),
+            ),
+        )
+        # Now we draw the agent
+        pygame.draw.circle(
+            canvas,
+            (0, 0, 255),
+            (self._agent_location + 0.5) * pix_square_size,
+            pix_square_size / 3,
+        )
+
+        for wall_pos in self.walls:
+            pygame.draw.rect(
+                canvas,
+                (0,0,0),
+                pygame.Rect(
+                    np.array(wall_pos) * pix_square_size,
+                    (pix_square_size, pix_square_size),
+                ),
+            )
+
+        # Finally, add some gridlines
+        for x in range(self.size + 1):
+            pygame.draw.line(
+                canvas,
+                0,
+                (0, pix_square_size * x),
+                (self.window_size, pix_square_size * x),
+                width=3,
+            )
+            pygame.draw.line(
+                canvas,
+                0,
+                (pix_square_size * x, 0),
+                (pix_square_size * x, self.window_size),
+                width=3,
+            )
+
+        if self.render_mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # We need to ensure that human-rendering occurs at the predefined framerate.
+            # The following line will automatically add a delay to keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
+
+env = GridEnv(render_mode="human")
+obs, _ = env.reset()
+
+done = False
+while not done:
+    action = env.action_space.sample()
+    obs, reward, done, truncated, info = env.step(action)
+    print(f"Action: {action}, Reward: {reward}, Done: {done}")
+
+
+env.close()
 
         
